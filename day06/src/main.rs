7571115f5f913303f06fc17f2a92683c6a06d675
Seefrass <se.mhfr@proton.me>
2024-12-06 17:08:04 +0100

    solve day06 part2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -4,6 +4,7 @@ struct Guard {
     pos: (i32, i32),
     direction: (i32, i32),
     visited: HashSet<(i32, i32)>,
+    visited_dir: HashSet<((i32, i32), (i32, i32))>,
 }
 
 impl Guard {
@@ -14,10 +15,12 @@ impl Guard {
 
     fn new(pos: (i32, i32)) -> Self {
         let visited: HashSet<(i32, i32)> = HashSet::new();
+        let visited_dir: HashSet<((i32, i32), (i32, i32))> = HashSet::new();
         Guard {
             pos,
             direction: Self::NORTH,
             visited,
+            visited_dir,
         }
     }
 
@@ -48,6 +51,50 @@ impl Guard {
         }
         true
     }
+
+    fn walk(&mut self, map: &Vec<&str>) {
+        while self.step(&map) {}
+    }
+
+    fn step_check_loop(&mut self, map: &Vec<&str>) -> Option<bool> {
+        let cur = self.pos;
+        let next = (cur.0 + self.direction.0, cur.1 + self.direction.1);
+
+        if next.0 < 0
+            || next.0 as usize >= map[cur.1 as usize].len()
+            || next.1 < 0
+            || next.1 as usize >= map.len()
+        {
+            self.visited_dir.insert((cur, self.direction));
+            return Some(false);
+        }
+
+        if map[next.1 as usize].chars().nth(next.0 as usize).unwrap() == '#' {
+            self.direction = match self.direction {
+                Guard::NORTH => Guard::EAST,
+                Guard::EAST => Guard::SOUTH,
+                Guard::SOUTH => Guard::WEST,
+                Guard::WEST => Guard::NORTH,
+                _ => panic!("currently facing invalid direction!"),
+            }
+        } else {
+            if self.visited_dir.contains(&(cur, self.direction)) {
+                return Some(true);
+            } else {
+                self.visited_dir.insert((cur, self.direction));
+                self.pos = next;
+            }
+        }
+        None
+    }
+
+    fn walk_check_loop(&mut self, map: &Vec<&str>) -> bool {
+        let mut val = self.step_check_loop(map);
+        while val.is_none() {
+            val = self.step_check_loop(map);
+        }
+        val.unwrap()
+    }
 }
 
 fn main() {
@@ -55,8 +102,6 @@ fn main() {
     let contents = fs::read_to_string(path).unwrap();
 
     let map: Vec<&str> = contents.split("\n").filter(|&s| s != "").collect();
-
-    println!("len: {}", map.len());
 
     let pos = (
         map.iter()
@@ -68,13 +113,28 @@ fn main() {
         map.iter().position(|r| r.contains("^")).unwrap() as i32,
     );
 
-    println!("pos: {:?}", pos);
-
     let mut guard = Guard::new(pos);
 
-    while guard.step(&map) {}
+    guard.walk(&map);
 
     let result = guard.visited.len();
 
     println!("Different locations Guard has been to: {}", result);
+
+    let mut path = guard.visited;
+    path.remove(&pos);
+
+    let result = path
+        .iter()
+        .filter(|p| {
+            let mut nmap = map.clone();
+            let mut string = nmap.get_mut(p.1 as usize).unwrap().to_string();
+            string.replace_range((p.0 as usize)..=(p.0 as usize), "#");
+            let _ = std::mem::replace(nmap.get_mut(p.1 as usize).unwrap(), string.as_str());
+            let mut guard = Guard::new(pos);
+            guard.walk_check_loop(&nmap)
+        })
+        .count();
+
+    println!("Possible ways of placing one Obstacle: {}", result);
 }
