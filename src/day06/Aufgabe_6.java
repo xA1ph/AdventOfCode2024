c3dfb1758f255ce3f831afc022233accaf377529
Alph <niklas146@gmx.de>
2024-12-08 23:17:27 +0100

    Tag 6 Teil 2 Versuch

--- src/day06/Aufgabe_6.java
+++ src/day06/Aufgabe_6.java
@@ -16,6 +16,11 @@ public class Aufgabe_6 {
 
 	private static char[][] matrix = new char[130][130];
 	private static Vector<Integer> curr = new Vector<>();
+	private static Vector<Integer> start = new Vector<>();
+
+	private static List<Vector<Integer>> vecList = new ArrayList<>();
+
+	private static boolean go = true;
 
 	public static void main(String[] args) {
 		Path path = Path.of("src/day06/Task06.txt");
@@ -33,44 +38,83 @@ public class Aufgabe_6 {
 				k++;
 			}
 
+			start = getCurrPos();
 			curr = getCurrPos();
-			int i = curr.getFirst();
-			int j = curr.getLast();
-			while (true) {
-				curr = getCurrPos();
-				while (!wallNorth(curr)) {
-					walk("N");
-					i--;
-				}
-				curr = getCurrPos();
-				while (!wallEast(curr)) {
-					walk("E");
-					j++;
-				}
-				curr = getCurrPos();
-				while (!wallSouth(curr)) {
-					walk("S");
-					i++;
-				}
-				curr = getCurrPos();
-				while (!wallWest(curr)) {
-					walk("W");
-					j--;
-				}
 
+			try {
+				while (go) {
+					curr = getCurrPos();
+					while (!wallNorth(curr)) {
+						walk("N");
+					}
+					curr = getCurrPos();
+					while (!wallEast(curr)) {
+						walk("E");
+					}
+					curr = getCurrPos();
+					while (!wallSouth(curr)) {
+						walk("S");
+					}
+					curr = getCurrPos();
+					while (!wallWest(curr)) {
+						walk("W");
+					}
+
+				}
+			} catch (Exception e) {
+				matrix[curr.getFirst()][curr.getLast()] = 'X';
+				System.out.println(findAllPos());
+				curr = start;
+				go = false;
 			}
 
 			// ------------------------------------- //
+			int t = 0;
+			outerloop: for (Vector<Integer> vec : vecList) {
+				boolean go2 = true;
+				curr = start;
+				matrix[curr.getFirst()][curr.getLast()] = '^';
+				matrix[vec.getFirst()][vec.getLast()] = '#';
+				try {
+					int i = 0;
+					while (go2) {
+						curr = getCurrPos();
+						while (!wallNorth(curr)) {
+							walk("N");
+						}
+						curr = getCurrPos();
+						while (!wallEast(curr)) {
+							walk("E");
+						}
+						curr = getCurrPos();
+						while (!wallSouth(curr)) {
+							walk("S");
+						}
+						curr = getCurrPos();
+						while (!wallWest(curr)) {
+							walk("W");
+						}
+						i++;
+						if (i > 5499) {
+							result2++;
+							go2 = false;
+						}
+					}
+				} catch (Exception e) {
+					continue outerloop;
+				}
+			}
+
+			System.out.println(curr);
+			System.out.println(t);
+			System.out.println(result2);
 
 		} catch (IOException e) {
 			System.out.println("Error");
-		} catch (Exception e) {
-			System.out.println(findAllPos() + 1);
 		}
-
 	}
 
-	private static Vector<Integer> getCurrPos() {
+	private static Vector<Integer> getCurrPos() throws IndexOutOfBoundsException {
 		Vector<Integer> curr = new Vector<>();
 		outerloop: for (int i = 0; i < 130; i++) {
 			for (int j = 0; j < 130; j++) {
@@ -84,7 +128,7 @@ public class Aufgabe_6 {
 		return curr;
 	}
 
-	private static void walk(String dir) {
+	private static void walk(String dir) throws IndexOutOfBoundsException {
 		if ("N".equals(dir)) {
 			curr.set(0, curr.getFirst() - 1);
 			curr.set(1, curr.getLast());
@@ -112,40 +156,46 @@ public class Aufgabe_6 {
 
 	}
 
-	private static boolean wallNorth(Vector<Integer> curr) {
+	private static boolean wallNorth(Vector<Integer> curr) throws IndexOutOfBoundsException {
 		if (matrix[curr.getFirst() - 1][curr.getLast()] == '#') {
 			return true;
 		}
 		return false;
 	}
 
-	private static boolean wallEast(Vector<Integer> curr) {
+	private static boolean wallEast(Vector<Integer> curr) throws IndexOutOfBoundsException {
 		if (matrix[curr.getFirst()][curr.getLast() + 1] == '#') {
 			return true;
 		}
 		return false;
 	}
 
-	private static boolean wallSouth(Vector<Integer> curr) {
+	private static boolean wallSouth(Vector<Integer> curr) throws IndexOutOfBoundsException {
 		if (matrix[curr.getFirst() + 1][curr.getLast()] == '#') {
 			return true;
 		}
 		return false;
 	}
 
-	private static boolean wallWest(Vector<Integer> curr) {
+	private static boolean wallWest(Vector<Integer> curr) throws IndexOutOfBoundsException {
 		if (matrix[curr.getFirst()][curr.getLast() - 1] == '#') {
 			return true;
 		}
 		return false;
 	}
 
-	private static int findAllPos() {
+	private static int findAllPos() throws IndexOutOfBoundsException {
 		int result = 0;
 		for (int i = 0; i < 130; i++) {
 			for (int j = 0; j < 130; j++) {
 				if (matrix[i][j] == 'X') {
+					Vector<Integer> vec = new Vector<>();
+					vec.addFirst(i);
+					vec.addLast(j);
 					result++;
+					if (!vec.equals(start)) {
+						vecList.add(vec);
+					}
 				}
 			}
 		}
